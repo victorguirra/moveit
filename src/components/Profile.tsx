b6ba2e78b0d91928bf3042ff2e33abe517d0ea16
victorguirra <victor.hugo.guirra@gmail.com>
2021-02-28 10:12:36 -0300

    sending all files

---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
+
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile(){
+    const { level } = useContext(ChallengesContext);
+
+    return(
+        <div className={ styles.profileContainer } >
+
+            <img src="https://github.com/victorguirra.png" alt="Victor Guirra" />
+
+            <div>
+
+                <strong>Victor Guirra</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level { level }
+                </p>
+
+            </div>
+ 
+        </div>
+    )
+}
